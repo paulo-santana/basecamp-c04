22f8548b9d0ba80be54a0e0bc97ec1ab0ed4cc79
Paulo Santana <paulo.santana.r@gmail.com>
2021-04-13 16:28:56 -0300

    ft putnbr base

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -10,6 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+char	*g_base_symbols;
+
 int		ft_strlen(char *str)
 {
 	int	size;
@@ -22,34 +26,53 @@ int		ft_strlen(char *str)
 
 int		is_base_valid(char *base)
 {
-	int i;
-	int	size;
-	char n;
-	char computed[100];
+	int		i;
+	int		size;
+	char	n;
+	char	computed[128];
 
 	size = ft_strlen(base);
 	if (size <= 1)
 		return (0);
-	while (base[size])
+	i = 0;
+	while (base[i])
 	{
-		n = base[size];
+		n = base[i];
 		if (n < 32 || n > 126 || n == '+' || n == '-')
 			return (0);
-		i = -1;
-		while (++i < size)
-		{
-			if (base[size] == computed[i])
-				return (0);
-		}
-		computed[i] = base[size];
-		size++;
+		if (n == computed[n])
+			return (0);
+		computed[base[i]] = base[i];
+		i++;
 	}
 	return (1);
 }
 
+void	convert(long int nbr, int base)
+{
+	char digit;
+
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr *= -1;
+	}
+	digit = nbr % base;
+	nbr = nbr / base;
+	if (nbr != 0)
+		convert(nbr, base);
+	write(1, &g_base_symbols[digit], 1);
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int size;
+	int base_size;
+	int is_valid;
 
-	size = is_base_valid(base);
+	is_valid = is_base_valid(base);
+	if (!is_valid)
+		return ;
+	base_size = ft_strlen(base);
+	g_base_symbols = base;
+	convert(nbr, base_size);
 }
